3b0a5034e20d79e42d23fbf9471d693e4342bcb5
Stéphane <stephane.pires.learning@gmail.com>
2021-12-01 23:00:33 +0100

    test(landing page): 🚧test apollo-client

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,56 @@ import {
     NumberInputStepper,
     NumberIncrementStepper,
     NumberDecrementStepper,
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
 } from '@chakra-ui/react'
 import Head from 'next/head'
 // import Link from 'next/link'
-// import { InferGetStaticPropsType } from 'next'
+import { InferGetStaticPropsType } from 'next'
+import { ApolloQueryResult, gql } from '@apollo/client'
 import Layout, { SITE_TITLE } from '../components/layout'
 import userAge from '../lib/user-data'
+import client from '../apollo-client'
+
+interface Countries {
+    code: string
+    name: string
+    emoji: string
+}
+
+interface Data {
+    countries: Countries[]
+}
 
 export async function getStaticProps() {
+    const { data }: ApolloQueryResult<Data> = await client.query({
+        query: gql`
+            query Countries {
+                countries {
+                    code
+                    name
+                    emoji
+                }
+            }
+        `,
+    })
+
     return {
-        props: {},
+        props: {
+            countries: data.countries.slice(0, 4),
+        },
     }
 }
 
-export default function Home() {
+export default function Home({
+    countries,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
     const [age, setAge] = useRecoilState(userAge)
 
     const parse = (val: string): number => Number(val)
@@ -27,6 +63,7 @@ export default function Home() {
     const onChange = (inputAge: string) => {
         setAge(parse(inputAge))
     }
+
     return (
         <Layout>
             <Head>
@@ -44,6 +81,35 @@ export default function Home() {
                     <NumberDecrementStepper />
                 </NumberInputStepper>
             </NumberInput>
+
+            <Table variant="simple">
+                <TableCaption>Countries</TableCaption>
+                <Thead>
+                    <Tr>
+                        <Th>Code</Th>
+                        <Th>Name</Th>
+                        <Th>Emoji by</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {countries &&
+                        countries.map(({ code, name, emoji }) => (
+                            <Tr key={code}>
+                                <Td>{code}</Td>
+                                <Td>{name}</Td>
+                                <Td>{emoji}</Td>
+                            </Tr>
+                        ))}
+                    )
+                </Tbody>
+                <Tfoot>
+                    <Tr>
+                        <Th>Code</Th>
+                        <Th>Name</Th>
+                        <Th>Emoji by</Th>
+                    </Tr>
+                </Tfoot>
+            </Table>
         </Layout>
     )
 }
