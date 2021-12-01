162c1dc781769a99174bd5d66e9d5b411f7f4cb0
Stéphane <stephane.pires.learning@gmail.com>
2021-12-01 13:05:40 +0100

    feat(dev-tooling): 🔧 add 'library' as a commit scope

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'scope-enum': [2, 'always', ['project', 'dev-tooling']],
+        'scope-enum': [2, 'always', ['project', 'dev-tooling', 'library']],
     },
 }
