9762be847ae2ea3548de60ca062ea64fea4e3e88
Stéphane <stephane.pires.learning@gmail.com>
2021-12-01 11:59:19 +0100

    feat(dev-tooling): ♻️ factorise the eslint  configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,54 @@
-module.exports = {
-    extends: [
-        'airbnb',
-        'airbnb/hooks',
-        'next/core-web-vitals',
-        'prettier',
-        'plugin:@next/next/recommended',
-    ],
-    rules: {
-        'react/function-component-definition': [
-            'error',
-            {
-                namedComponents: 'function-declaration',
-                unnamedComponents: 'arrow-function',
-            },
-        ],
-        'jsx-a11y/anchor-is-valid': 'off',
-    },
-    overrides: [
+// rules that overrides extends
+const RULES = {
+    'react/function-component-definition': [
+        'error',
         {
-            extends: [
-                'airbnb',
-                'airbnb/hooks',
-                'airbnb-typescript',
-                'next/core-web-vitals',
-                'prettier',
-                'plugin:@next/next/recommended',
-            ],
-            files: ['*.ts', '*.tsx'],
-            parserOptions: {
-                project: ['./tsconfig.json'],
-            },
-            rules: {
-                'react/function-component-definition': [
-                    'error',
-                    {
-                        namedComponents: 'function-declaration',
-                        unnamedComponents: 'arrow-function',
-                    },
-                ],
-                'jsx-a11y/anchor-is-valid': 'off',
-            },
+            namedComponents: 'function-declaration',
+            unnamedComponents: 'arrow-function',
         },
     ],
+    'jsx-a11y/anchor-is-valid': 'off',
+}
+
+// configration extends techno
+const EXTENDS_AIRBNB_JAVASCRIPT = ['airbnb', 'airbnb/hooks']
+
+const EXTENDS_AIRBNB_TYPESCRIPT = ['airbnb-typescript']
+
+const EXTENDS_NEXT = ['next/core-web-vitals', 'plugin:@next/next/recommended']
+
+const EXTENDS_PRETTIER = ['prettier']
+
+// configuration extends file types
+const EXTENDS_JAVASCRIPT = [
+    ...EXTENDS_AIRBNB_JAVASCRIPT,
+    ...EXTENDS_NEXT,
+    ...EXTENDS_PRETTIER,
+]
+
+const EXTENDS_TYPESCRIPT = [
+    ...EXTENDS_AIRBNB_JAVASCRIPT,
+    ...EXTENDS_AIRBNB_TYPESCRIPT,
+    ...EXTENDS_NEXT,
+    ...EXTENDS_PRETTIER,
+]
+
+// overrides depending on language
+const TYPESCRIPT = [
+    {
+        extends: EXTENDS_TYPESCRIPT,
+        files: ['*.ts', '*.tsx'],
+        parserOptions: {
+            project: ['./tsconfig.json'],
+        },
+        rules: RULES,
+    },
+]
+
+// configuration
+module.exports = {
+    extends: EXTENDS_JAVASCRIPT,
+    rules: RULES,
+    overrides: TYPESCRIPT,
     parser: '@typescript-eslint/parser',
 }
