b048445e11867b89781bedbc0c86f0a50f37d0ef
Stéphane <stephane.pires.learning@gmail.com>
2021-12-07 19:19:51 +0100

    refactor(graphql): ⚰️ delete  dead commented code

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,24 +1,3 @@
-// graphql/schema.ts
-// import { gql } from 'apollo-server-micro'
-
-// The graphQL schema passed to Apollo Server to define the types
-// const typeDefs = gql`
-//     type Link {
-//         id: Int
-//         name: String
-//         description: String
-//         url: String
-//         category: String
-//         imageUrl: String
-//     }
-
-//     type Query {
-//         links: [Link]!
-//     }
-// `
-
-// export default typeDefs
-
 // /graphql/schema.ts
 import { makeSchema } from 'nexus'
 import { GraphQLSchema } from 'graphql'
