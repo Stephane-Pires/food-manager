5b9cdfb94ee130e887fd36d38d8c6bea4ac2e4dd
Stéphane <stephane.pires.learning@gmail.com>
2021-12-08 00:00:25 +0100

    feat(food book): 🤡 mock the page

--- pages/food-book.tsx
+++ pages/food-book.tsx
@@ -1,9 +1,46 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import { Button } from '@chakra-ui/button'
+import { Box, Center, Flex, SimpleGrid, Text } from '@chakra-ui/layout'
+
 import Layout from '../components/layout'
 
 export default function FoodBook() {
     return (
         <Layout>
-            <div />
+            <Head>
+                <title>Food Book</title>
+            </Head>
+            <Center>
+                <Flex direction="column">
+                    <Text fontSize="6xl">Food Book</Text>
+                    <Text fontSize="xl">Index of recipes</Text>
+                </Flex>
+            </Center>
+
+            <SimpleGrid columns={6} spacing={10}>
+                <Box bg="tomato" height="200px" width="200px" />
+                <Box bg="tomato" height="200px" width="200px" />
+                <Box bg="tomato" height="200px" width="200px" />
+                <Box bg="tomato" height="200px" width="200px" />
+                <Box bg="tomato" height="200px" width="200px" />
+                <Box bg="tomato" height="200px" width="200px" />
+                <Box bg="tomato" height="200px" width="200px" />
+                <Box bg="tomato" height="200px" width="200px" />
+                <Box bg="tomato" height="200px" width="200px" />
+                <Box bg="tomato" height="200px" width="200px" />
+            </SimpleGrid>
+
+            <Center>
+                <Flex direction="column">
+                    <Link href="/" passHref>
+                        <Button variant="outline" size="lg">
+                            Tech Stack
+                        </Button>
+                    </Link>
+                </Flex>
+            </Center>
         </Layout>
     )
 }
