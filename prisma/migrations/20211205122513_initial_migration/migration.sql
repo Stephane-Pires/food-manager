b1d1821ff46103c6c6af77ff36255e0a1c3ec307
Stéphane <stephane.pires.learning@gmail.com>
2021-12-08 15:33:55 +0100

    chore(database): 🗃️ suppress migration history

--- prisma/migrations/20211205122513_initial_migration/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Link" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-    "imageUrl" TEXT NOT NULL,
-    "category" TEXT NOT NULL
-);
