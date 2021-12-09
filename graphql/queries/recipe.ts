6708ca6bdf07884ce4c3010cf1e29cb1a0f8c3b5
Stéphane <stephane.pires.learning@gmail.com>
2021-12-09 12:16:24 +0100

    feat(graphql): 🚧 update apollo cliient AllRcipes query

--- graphql/queries/recipe.ts
+++ graphql/queries/recipe.ts
@@ -9,8 +9,6 @@ const AllRecipes = {
                 description
                 url
                 category
-                createdAt
-                updatedAt
             }
         }
     `,
