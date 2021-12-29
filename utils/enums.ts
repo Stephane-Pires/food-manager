8a887a299305626233537133f77f418ceebd29ee
Stéphane <stephane.pires.learning@gmail.com>
2021-12-29 12:52:49 +0100

    feat(utils): ✨ add a hook "useEnumInfo" & add enum helpers

--- utils/enums.ts
+++ utils/enums.ts
@@ -18,3 +18,31 @@ export function getEnumFromEnumKey(enumKey: EnumKey) {
             return 'Return a proper error :/'
     }
 }
+
+// Predicates
+export function isService(enumItem: any) {
+    return Object.values(Service).includes(enumItem)
+}
+
+export function isDiet(enumItem: any) {
+    return Object.values(Diet).includes(enumItem)
+}
+
+export function isStack(enumItem: any) {
+    return Object.values(Stack).includes(enumItem)
+}
+
+// Reducer
+export function reduceEnumItem(enumItem, ifService, ifDiet, ifStack) {
+    if (isService(enumItem)) {
+        return ifService(enumItem)
+    }
+    if (isDiet(enumItem)) {
+        return ifDiet(enumItem)
+    }
+    if (isStack(enumItem)) {
+        return ifStack(enumItem)
+    }
+
+    return 'Return a proper error :/'
+}
